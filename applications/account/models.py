9bdc08072fffda0499bec22505607c11419e2173
Jamal130613 <liyapark.com@gmail.com>
2022-07-26 21:14:57 +0600

    customuser

--- applications/account/models.py
+++ applications/account/models.py
@@ -1,3 +1,60 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class UserManager(BaseUserManager):
+
+    def _create_user(self, username, email, password, **extra_fields):
+        if not email:
+            raise ValueError("The given email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.create_activation_code()
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, username, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(username, email, password, **extra_fields)
+
+
+class CustomUser(AbstractUser):
+    username = models.CharField(max_length=30, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+    activation_code = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=False)
+    last_name = models.CharField(max_length=50)
+
+    USERNAME_FIELDS = 'username'
+
+    REQUIRED_FIELDS = ['email',]
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.username
+
+    def create_activation_code(self):
+        import uuid
+        code = str(uuid.uuid4())
+        self.activation_code = code
+
+    def generate_activation_code(self):
+        import uuid
+        code = str(uuid.uuid4())
+        self.activation_code = code
